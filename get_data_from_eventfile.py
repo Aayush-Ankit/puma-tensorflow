1635b2787ed1f52fec318f4fadc1dfc8d1088bba
Aayush Ankit <aankit@cbric-gpu3.ecn.purdue.edu>
2018-12-03 22:45:50 -0500

    added file to extract data from tf events

---
+++ get_data_from_eventfile.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+#purdue sim
+#path = '/local/scratch/a/aankit/tensorflow/approx_memristor/cifar100/vgg16_memristor_nonideality/puma_vgg16_slice_mixed_55554444_crsfreq_4/'
+#file_name = 'events.out.tfevents.1543592396.cbric-gpu3.ecn.purdue.edu'
+
+# hpe sim
+path = '/local/scratch/a/aankit/tensorflow/approx_memristor/cifar100/vgg16_memristor_nonideality/simulationdata/puma_vgg16_slice_3bits_crsfreq_256/'
+file_name = 'events.out.tfevents.1543363584.deepsim-10.labs.hpecorp.net'
+
+events_file_path = path + file_name
+
+for e in tf.train.summary_iterator(events_file_path):
+    for v in e.summary.value:
+        if v.tag == 'Loss':
+            print(v.simple_value)
