901f7f755a6d193e4be7497d43a321ebe13db1eb
Aayush Ankit <aankit@cbric-gpu3.ecn.purdue.edu>
2018-12-07 16:50:55 -0500

    added mlp-l4 benchmark

--- get_data_from_eventfile.py
+++ get_data_from_eventfile.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 
 #purdue sim
-#path = '/local/scratch/a/aankit/tensorflow/approx_memristor/cifar100/vgg16_memristor_nonideality/puma_vgg16_slice_mixed_55554444_crsfreq_4/'
-#file_name = 'events.out.tfevents.1543592396.cbric-gpu3.ecn.purdue.edu'
+path = '/local/scratch/a/aankit/tensorflow/approx_memristor/cifar100/vgg16_memristor_nonideality/puma_vgg16_slice_mixed_55554444_crsfreq_4/'
+file_name = 'events.out.tfevents.1543592396.cbric-gpu3.ecn.purdue.edu'
 
 # hpe sim
-path = '/local/scratch/a/aankit/tensorflow/approx_memristor/cifar100/vgg16_memristor_nonideality/simulationdata/puma_vgg16_slice_3bits_crsfreq_256/'
-file_name = 'events.out.tfevents.1543363584.deepsim-10.labs.hpecorp.net'
+#path = '/local/scratch/a/aankit/tensorflow/approx_memristor/cifar100/vgg16_memristor_nonideality/simulationdata/puma_vgg16_slice_3bits_crsfreq_256/'
+#file_name = 'events.out.tfevents.1543363584.deepsim-10.labs.hpecorp.net'
 
 events_file_path = path + file_name
 
